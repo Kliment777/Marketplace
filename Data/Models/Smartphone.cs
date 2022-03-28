a44bfce128e09b0489152729ea436f2d57358e56
Kliment_Kirillov <cklim.kirillov@yandex.ru>
2022-03-29 00:32:43 +0300

    создал два класса, Category и Smarfone

---
+++ Data/Models/Smartphone.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Мarketplace.Data.Models
+{
+    public class Smartphone //класс смартфон 
+    {
+    }
+}
---
+++ Data/Models/Сategory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Мarketplace.Data.Models 
+{
+    public class Сategory 
+    {
+        public int id { get; set; } 
+        public string categoryName { get; set; } // название категории 
+        public string description { get; set; } // описание
+
+        public List<Smartphone> Electronics { get; set; } //электроника
+    }
+}
