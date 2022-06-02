e204d945753e2cd1d293801e40cb3a4122984c3a
Kliment_Kirillov <cklim.kirillov@yandex.ru>
2022-06-02 14:47:12 +0300

    Создали базу данных.

---
+++ Migrations/20220602111052_Initial.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Мarketplace.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Сategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Сategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Smarphone",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Designation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ShortDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LongDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Img = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<long>(type: "bigint", nullable: false),
+                    IsFavourite = table.Column<bool>(type: "bit", nullable: false),
+                    Available = table.Column<bool>(type: "bit", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Smarphone", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Smarphone_Сategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Сategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Smarphone_CategoryId",
+                table: "Smarphone",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Smarphone");
+
+            migrationBuilder.DropTable(
+                name: "Сategories");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,29 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Мarketplace.Data.Db;
 using Мarketplace.Data.Interfaces;
 using Мarketplace.Data.Mocks;
+using Мarketplace.Data.Repository;
 
 namespace Мarketplace
 {
     public class Startup
     {
+        private IConfigurationRoot _confstring;
+        public Startup(IWebHostEnvironment hostEnv) 
+        {
+            _confstring = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<ISmartphoneCategory, MockCategory>();
-            services.AddTransient<IAllSmartphones, MockSmartphones>();
+            services.AddDbContext<AppDBContent>(options => options.UseSqlServer(_confstring.GetConnectionString("DefaultConnection")));
+            /*services.AddTransient<ISmartphoneCategory, MockCategory>();
+            services.AddTransient<IAllSmartphones, MockSmartphones>();*/
+            services.AddTransient<IAllSmartphones, SmatphoneRepository>();
+            services.AddTransient<ISmartphoneCategory, CategoryRepository>();
             services.AddMvc();
         }
 
